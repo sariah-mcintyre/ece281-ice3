6711fd8d66abcd52bf6888033b593af71014e99f
sariah-mcintyre <sariah.mcintyre@gmail.com>
2025-02-13 19:04:42 -0700

    fourth try

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -1,4 +1,4 @@
---+----------------------------------------------------------------------------
+------------------------------------------------------------------------------
 --| Testbench for 4-bit Ripple-Carry Adder
 --+----------------------------------------------------------------------------
 library ieee;
@@ -59,8 +59,18 @@ begin
 	   w_addends <= x"FF"; w_Cin <= '0'; wait for 10 ns;
 	       assert (w_sum = x"E" and w_Cout = '1') report "bad with inTB" severity failure;
 	       
-	   --w_addends <= x"E1"; w_Cin <= '1'; wait for 10 ns;
-	       --assert (w_sum = x"F" and w_Cout = '0') report "bad with random" severity failure;
+	       
+	   w_addends <= x"A0"; w_Cin <= '0'; wait for 10 ns;
+        assert (w_sum = x"A" and w_Cout = '0') report "bad with 0000 + 1010" severity failure;
+       --
+       w_addends <= x"1E"; w_Cin <= '1'; wait for 10 ns;
+        assert (w_sum = x"0" and w_Cout = '1') report "bad with 1110 + 0001" severity failure;
+       --
+       w_addends <= x"5A"; w_Cin <= '0'; wait for 10 ns ;
+        assert (w_sum = x"F" and w_Cout = '0') report "bad with 1010 + 0101" severity failure ;
+       --
+       w_addends <= x"DD"; w_Cin <= '0'; wait for 10 ns;
+        assert (w_sum = x"A" and w_Cout = '1') report "bad with 1101 + 1101" severity failure;
 	   
 	
 		wait; -- wait forever
