b752a8439da33a099e05168d0e87771a428ceb68
sariah-mcintyre <sariah.mcintyre@gmail.com>
2025-02-13 17:45:03 -0700

    first try

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -47,8 +47,17 @@ begin
 	       assert (w_sum = x"0" and w_Cout = '0') report "bad with zeros" severity failure;
        -- Test all ones input
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
-	       assert (w_sum = x"E" and w_Cout = '1') report "bad with ones" severity failure;
+	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
+       w_addends <= x"0F"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"F" and w_Cout = '0') report "bad with random" severity failure;
+	       
+	   w_addends <= x"DC"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"9" and w_Cout = '1') report "bad with random" severity failure;
+	       
+	   w_addends <= x"E1"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"F" and w_Cout = '0') report "bad with random" severity failure;
+	   
 	
 		wait; -- wait forever
 	end process;	
